733b1f358f1ca06c94c9b0e4d48d653e7ba944b7
DmytroKatruk <dmitriykatrukk@gmail.com>
2023-07-30 22:54:05 +0300

    change grid server address

--- src/test/java/dou/DouTest.java
+++ src/test/java/dou/DouTest.java
@@ -26,7 +26,7 @@ public class DouTest {
                 .readTimeout(Duration.ofMinutes(20)); // I change this 3 minute(Default) to 20 minutes.
         DesiredCapabilities caps = new DesiredCapabilities("chrome", "", Platform.ANY);
 
-        driver = RemoteWebDriver.builder().oneOf(caps).address("http://172.23.0.3:5555").config(config).build(); // now you can use this remoteWebDriver.
+        driver = RemoteWebDriver.builder().oneOf(caps).address("http://localhost:4444/").config(config).build(); // now you can use this remoteWebDriver.
 
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)).pageLoadTimeout(Duration.ofSeconds(10));
